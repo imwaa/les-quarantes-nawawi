6bf3bb8c4e35080eaf67a46e4e7992b34241ada0
Walid Salhi <walid.dev@outlook.fr>
2022-02-24 16:01:31 +0100

    changing status bar color in white

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StatusBar } from '@capacitor/status-bar';
+import { isPlatform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    if(isPlatform('mobile') && isPlatform('pwa')){
+      StatusBar.setBackgroundColor({color:'#fff'})
+    }
+  }
 }
