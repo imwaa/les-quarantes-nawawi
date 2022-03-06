43e7e535af2690ef718b03eae5535dfa20a6c9e9
Salhi Walid <walid.dev@outlook.fr>
2022-03-06 16:58:51 +0100

    Ajout des hadfith jusqu'au 42, fin !!

--- src/app/services/hadith-service.service.ts
+++ src/app/services/hadith-service.service.ts
@@ -12,7 +12,7 @@ export class HadithServiceService {
 
   getAllHadith(): HadithList[] {
     const hadithList: HadithList[] = [];
-    for (let i = 0; i < 30; i++) {
+    for (let i = 0; i < 42; i++) {
       hadithList.push(this.getOneHadith(i));
     }
     return hadithList;
