47360e1147fe6c60711a8bf5f777e24dfd4fa59b
walid salhi <walid.dev@outlook.fr>
2022-04-25 10:19:50 +0200

    ajout des derniers modif de config android & modification des social buttons

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.walid.nawawi',
-  appName: '40 Hadith',
+  appName: '40 Nawawi',
   webDir: 'www',
   bundledWebRuntime: false
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { StatusBar } from '@capacitor/status-bar';
-import { isPlatform } from '@ionic/angular';
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { SplashScreen } from '@capacitor/splash-screen';
+
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,17 @@ import { isPlatform } from '@ionic/angular';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
+  
+  async initializeApp() {
+    /* To make sure we provide the fastest app loading experience 
+       for our users, hide the splash screen automatically 
+       when the app is ready to be used:
+
+        https://capacitor.ionicframework.com/docs/apis/splash-screen#hiding-the-splash-screen
+    */
+    await SplashScreen.hide();
+    await StatusBar.setStyle({ style: Style.Default });
+    
+  }
 }
