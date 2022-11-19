90f2563693ebdeba32c2d773575a5d16bc14eb38
walid <walid.dev@outlook.fr>
2022-11-19 15:11:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hadith-nawawi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
